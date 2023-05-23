ad0ad7f6f309e87a6d7cf8350afe473a5f36a925
青柳　恭義 <takayoshi_aoyagi@waku-2.com>
2023-05-23 16:57:40 +0900

    support new google form format

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// 学童名
+const GAKUDO_NAME =
+
 // 各学童用の Webhook URL
-const WEBHOOK_URL = 
+const WEBHOOK_URL =
 // 土曜保育用の Webhook URL
-const WEBHOOK_URL_SAT = 
+const WEBHOOK_URL_SAT =
 
 
 function isEmpty(x) {
@@ -15,39 +18,26 @@ function getValue(e, key) {
   return e.namedValues[key][0];
 }
 
-function getFirstChild(e) {
-  return {
-    'name': getValue(e, '児童1　氏名'),
-    'grade': getValue(e, '児童1　学年')
+function getChildren(e) {
+  const grades = ['1', '2', '3'];
+  const children = [];
+  for (const grade of grades) {
+    const name = getValue(e, `児童${grade}　氏名`);
+    if (isEmpty(name)) {
+      continue;
+    }
+    const child =  {
+      'name': name,
+      'grade': getValue(e, `児童${grade}　学年`)
+    }
+    children.push(child);
   }
+  return children;
 }
 
-// function getOtherChildren(e) {
-//   const ordinals = ['2', '3'];
-//   const children = [];
-//   for (ordinal of ordinals) {
-//     const child = {
-//       'name': e.namedValues[`児童${ordinal}　氏名`],
-//       'grade': getGrade(`児童${ordinal}`);
-//     }
-//   }
-// }
-
-/**
- * 	Google Form (Spreadsheet) から送られてくる情報を欠席情報に変換する
- */
-function getItem(e) {
-  const item = {};
-  item.ts = getValue(e, 'タイムスタンプ');
-
-  const child_1 = getFirstChild(e);
-  // const others = getOtherChildren();
-  // const children = [child_1].extend(others);
-  item.children = [child_1]
-
-  item.reason = getValue(e, '欠席理由');
-  item.dates = [];
-  item.saturdays = [];
+function getDates(e) {
+  const dates = [];
+  const saturdays = [];
   const keys = ['欠席日 1', '欠席日 2', '欠席日 3', '欠席日 4', '欠席日 5'];
   for (const key of keys) {
     const dt = getValue(e, key);
@@ -57,24 +47,41 @@ function getItem(e) {
     const absDate = new Date(dt);
     console.log(absDate, absDate.getDay())
     if (absDate.getDay() === 6) {
-      item.saturdays.push(dt)
+      saturdays.push(dt)
     } else {
-      item.dates.push(dt)
+      dates.push(dt)
     }
   }
+  return [dates, saturdays];
+}
+
+/**
+ * Google Form (Spreadsheet) から送られてくる情報を欠席情報に変換する
+ */
+function getItem(e) {
+  const item = {};
+  item.ts = getValue(e, 'タイムスタンプ');
+  item.children = getChildren(e);
+  item.reason = getValue(e, '欠席理由');
+  const [dates, saturdays] = getDates(e);
+  item.dates = dates;
+  item.saturdays = saturdays;
   return item;
 }
 
 /**
  * 通知メッセージ文字列を作成する
  */
-function getMessage(child, dates, reason, ts) {
+function getMessage(child, dates, reason, ts, gakudoName) {
   const msgs = []
-  msgs.push(`児童氏名：${child.name}`);
-  msgs.push(`児童学年：${child.grade}`);
-  msgs.push(`欠席日時：${dates}`);
-  msgs.push(`欠席理由：${reason}`);
-  msgs.push(`送信日時：${ts}`);
+  if (gakudoName) {
+    msgs.push(`所属学童： ${gakudoName}`);
+  }
+  msgs.push(`児童氏名： ${child.name}`);
+  msgs.push(`児童学年： ${child.grade}`);
+  msgs.push(`欠席日時： ${dates}`);
+  msgs.push(`欠席理由： ${reason}`);
+  msgs.push(`送信日時： ${ts}`);
   return msgs.join('\n');
 }
 
@@ -86,12 +93,12 @@ function getMessages(item) {
   const satMsgs = [];
   const dates = item.dates.concat(item.saturdays);
   for (const child of item.children) {
-    const msg = getMessage(child, dates, item.reason, item.ts)
+    const msg = getMessage(child, dates, item.reason, item.ts, null)
     msgs.push(msg);
   }
   if (item.saturdays.length > 0) {
     for (const child of item.children) {
-      const msg = getMessage(child, item.saturdays, item.reason, item.ts)
+      const msg = getMessage(child, item.saturdays, item.reason, item.ts, GAKUDO_NAME)
       satMsgs.push(msg);
     }
   }
